4bbab3b2f5ee4ddba5413d7904f39a2d86f5ec9b
Federico Moschini F <federico.moschini02@gmail.com>
2026-02-04 15:00:07 +0000

    margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,4 @@
+WITH cost_temp AS(
 SELECT
 sales.*
 , purchase_price
@@ -5,3 +6,8 @@ sales.*
 FROM {{ ref("stg_gz_raw_data__product")}} AS product
 RIGHT JOIN {{ ref("stg_gz_raw_data__sales")}} AS sales
 ON product.products_id = sales.product_id
+)
+SELECT
+*,
+revenue - purchase_cost AS margin
+FROM cost_temp
